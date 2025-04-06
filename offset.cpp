4cf3a729fd30e935f9d453445fe4c44be206dea8
JamesDevoloper <154053203+JamesDevoloper@users.noreply.github.com>
2025-04-06 22:28:03 +0300

    Create offset.cpp

---
+++ offset.cpp
@@ -0,0 +1,19 @@
+STANDOFF 0.33.1
+
+0x444190C High Jump
+0x443A13C FireRate
+0x443A37C Recoil
+0x444151C Friendly Fire
+0x444187C Only Headshot 
+0x4441A2C Infinity Ammo
+0x4441C6C Infinity Buy Time
+0x444283C Max Health 
+0x4111A10 MVP Terrorist 
+0x4111AA0 MVP Counter-terrorist
+0x4119640 check mark (verification account)
+0x411A210 Ranked match banned 
+0x4120D10 kill in stats 
+0x41214F0 StatTrack kills 
+0x5F1F598 Added Inventory
+
+Good luck!
